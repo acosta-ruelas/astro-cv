96dfc63129508e222b897e7a49d615169043ffbd
acosta-ruelas <159663364+acosta-ruelas@users.noreply.github.com>
2024-04-14 19:48:28 -0600

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    // site: "https://github.com/acosta-ruelas/astro-cv",
+    // base: "https://github.com/acosta-ruelas",
+});
